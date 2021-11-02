30e6d388568e5b4a6285170b46dc8802df22d71a
Love <verma.91love@gmail.com>
2021-11-02 22:38:44 +0530

    New Token Updating

--- SocialMediaApp/Helper/KeyConstant.swift
+++ SocialMediaApp/Helper/KeyConstant.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct KeyConstant {
     enum ApplicationKeys: String {
-        case gitHubAuthToken = "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"
+        case gitHubAuthToken = "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"
     }
     
     enum FollowListType {
